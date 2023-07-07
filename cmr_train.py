ffa17924ca6c9a57b8a61e4ce40a5ff21d638fa4
Sascha Stocker <saschastocker@DNa20767b.SUNet>
2023-07-07 09:04:48 -0700

    stack 2D iamges to see 3D nii.gz file

--- cmr_train.py
+++ cmr_train.py
@@ -42,42 +42,35 @@ iter_counter = IterationCounter(opt, len(dataloader))
 # create tool for visualization
 visualizer = Visualizer(opt)
 
-
-def inspect_dictionary(dictionary,i):
-    print(f' the dictonary for the {i} iteration')
-
-    for key, value in dictionary.items():
-        if key == 'image':
-            print(f'{value.shape}')
-        print(f"Key: {key}")
-        print(f"Value: {value}\n")
     
 
 for epoch in iter_counter.training_epochs():
     print('epoch', epoch)
     iter_counter.record_epoch_start(epoch)
 
-    image3D_epoch = torch.empty(0)
-
+    
 
     print(f'lenght of dataloader: {len(dataloader)}')
     for i, data_i in enumerate(dataloader, start=iter_counter.epoch_iter):
 
+        #First initalisation
+
         #look at data that's loaded:
-        print(f'i: {i}')
+        # print(f'i: {i}')
         #print(f' data_i: {data_i.keys()}')
         # print the value of the key with 'image'
 
-        #data_i dict_keys(['label', 'image', 'instance', 'dist', 'path', 'gtname', 'index', 'segpair_slice'])
-        print(f' data_i: {data_i["image"]}')
-        print(f' image shape: {data_i["image"].shape}')
-        print(f'gt name is: {data_i["gtname"]}')
-        print(f'path is: {data_i["path"]}')
+        # data_i dict_keys(['label', 'image', 'instance', 'dist', 'path', 'gtname', 'index', 'segpair_slice'])
+        # print(f' data_i: {data_i["image"]}')
+        # print(f' image shape: {data_i["image"].shape}')
+        # print(f'gt name is: {data_i["gtname"]}')
+        # print(f'path is: {data_i["path"]}')
+
+        #Set initial path:
+
+        #When paths change, a new 3D volume is being generated
 
 
-
-
-        #inspect_dictionary(data_i,i)
         iter_counter.record_one_iteration()
 
         # Training
@@ -101,14 +94,50 @@ for epoch in iter_counter.training_epochs():
                                    ('real_image', data_i['image'])])
             visualizer.display_current_results(visuals, epoch, iter_counter.total_steps_so_far)
 
+            #Save 3D stacked image
+
+
         #stack the images togehter to create a 3D image of the generated images
-        image3D_epoch = torch.cat((image3D_epoch,trainer.get_latest_generated()),0)
+
 
 
         if iter_counter.needs_saving():
             print('saving the latest model (epoch %d, total_steps %d)' %
                   (epoch, iter_counter.total_steps_so_far))
+        
             trainer.save('latest')
+        
+
+        if(i==0):
+            path = data_i['path']
+            #Expected 3D
+            image3D_epoch = torch.empty(221,512,512)
+            print(f'initial done')
+            print(f'image3D_epoch: {image3D_epoch.shape}')
+
+
+        elif(path != data_i['path'] and iter_counter.needs_displaying ):
+            #save old 3D stacked, should be 221 images stacked together
+            print(f'stacked image shape: {image3D_epoch.shape()}')
+            #Override for the new 3D stacked image
+            img = nib.Nifti1Image(image3D_epoch, affine)
+
+            #get image nr. from path file name
+            imgNr= data['path'][3:7]
+            filename = "3Depoch{epoch}Image{imgNr}.nii.gz"
+            nib.save(nifti_image, os.path.join(opt.checkpoints_dir, opt.name, 'images', filename))
+
+            # start new stacking for the next 3D image
+            image3D_epoch = torch.empty(221,512,512)
+            image3D_epoch[0,:,:] = trainer.get_latest_generated()[0,0,:,:]
+            path = data_i['path']
+        
+        else:
+            print(f'image_3d_epoch shape= {image3D_epoch.shape}')
+            print(f'latest generated is: {trainer.get_latest_generated().shape}')
+
+            image3D_epoch[i%221,:,:] = trainer.get_latest_generated()[0,0,:,:]
+
 
 
     trainer.update_learning_rate(epoch)
@@ -124,7 +153,7 @@ for epoch in iter_counter.training_epochs():
     image3D_epoch = image3D_epoch.cpu().detach().numpy()
     print(f' shape of image3D_epoch: {image3D_epoch.shape}')
     nifti_image = nib.Nifti1Image(image3D_epoch, affine=np.eye(4))
-    nib.save(nifti_image, os.path.join(opt.checkpoints_dir, opt.name, 'images', f'epoch_{epoch}.nii.gz'))
+    
 
 
 
