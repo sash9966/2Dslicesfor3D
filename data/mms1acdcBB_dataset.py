2cd2eb450436f460a0c1f7d4a4d2254d4d591e41
sash9966 <sascha.stocker@hotmail.ch>
2023-08-10 15:31:58 -0700

    path issue sherlock

--- data/mms1acdcBB_dataset.py
+++ data/mms1acdcBB_dataset.py
@@ -35,9 +35,9 @@ class Mms1acdcBBDataset(BaseDataset):
         parser.set_defaults(add_dist=False)
         
 
-        parser.add_argument('--label_dir', type=str, required=False, default = "/home/sastocke/data/alltrainingdata/data/segmentation",
+        parser.add_argument('--label_dir', type=str, required=False, default = "/scratch/users/sastocke/data/data/segmentation",
                             help='path to the directory that contains label images')
-        parser.add_argument('--image_dir', type=str, required=False, default ="/home/sastocke/data/alltrainingdata/data/images" ,
+        parser.add_argument('--image_dir', type=str, required=False, default ="/scratch/users/sastocke/data/data/images" ,
                             help='path to the directory that contains photo images')
         
         
