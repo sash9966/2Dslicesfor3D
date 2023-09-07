1b1496e6033cca8a82e04460a54a3734cd66b547
sash9966 <sascha.stocker@hotmail.ch>
2023-09-06 22:55:38 -0700

    save network pathsave update

--- util/util.py
+++ util/util.py
@@ -409,7 +409,7 @@ def find_class_in_module(target_cls_name, module):
 
 def save_network(net, label, epoch, opt):
     save_filename = '%s_net_%s.pth' % (epoch, label)
-    save_path = os.path.join(opt.checkpoints_dir, opt.name, save_filename)
+    save_path = os.path.join(opt.checkpoints_dir, save_filename)
     torch.save(net.cpu().state_dict(), save_path)
     if len(opt.gpu_ids) and torch.cuda.is_available():
         net.cuda()
@@ -420,7 +420,7 @@ def save_network(net, label, epoch, opt):
 
 def load_network(net, label, epoch, opt):
     save_filename = '%s_net_%s.pth' % (epoch, label)
-    save_dir = opt.checkpoints_dir
+    save_dir = os.path.join(opt.checkpoints_dir)
     save_path = os.path.join(save_dir, save_filename)
     weights = torch.load(save_path)
     net.load_state_dict(weights)
